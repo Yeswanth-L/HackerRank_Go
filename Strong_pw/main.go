57d6f005b8f511f037bfe0bcce9e40561157075e
Yeswanth-L <yeswanth2412@gmail.com>
2025-04-08 09:12:11 +0530

    storng_password

---
+++ Strong_pw/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+    "math"
+)
+
+func minimumNumber(n int, password string) int {
+	// Return the minimum number of characters to make the password strong
+	res := make(map[string]bool)
+	res["numbers"] = false
+	res["lower_case"] = false
+	res["upper_case"] = false
+	res["special_characters"] = false
+
+	numbers := "0123456789"
+	lower_case := "abcdefghijklmnopqrstuvwxyz"
+	upper_case := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special_characters := "!@#$%^&*()-+"
+	for i := 0; i < len(password); i++ {
+		if strings.Contains(numbers, string(password[i])) {
+			res["numbers"] = true
+		}
+		if strings.Contains(lower_case, string(password[i])) {
+			res["lower_case"] = true
+		}
+		if strings.Contains(upper_case, string(password[i])) {
+			res["upper_case"] = true
+		}
+		if strings.Contains(special_characters, string(password[i])) {
+			res["special_characters"] = true
+		}
+	}
+	
+	c := 0
+	for _, val := range res {
+		if !val {
+			c++
+		}
+	}
+    
+    return int(math.Max(float64(c), float64(6-n)))
+}
+
+func main() {
+	var n int
+	var str string
+	fmt.Scan(&n)
+	fmt.Scan(&str)
+	fmt.Println(minimumNumber(n,str))
+}
